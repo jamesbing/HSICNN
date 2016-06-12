9b6e749d52f50d9d038338a111ae65c9c8b6bcbe
jamesbing <lengjiabing@gmail.com>
2016-06-12 15:13:41 +0800

    HSICNN.py

--- Keras/HSICNN/HSICNN.py
+++ Keras/HSICNN/HSICNN.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from __future__ import print_function
+import numpy
+
+from keras.models import Sequential
+from keras.layers.core import Dense,Dropout,Activation,Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD
+
+from keras.utils import np_utils
+
+
+
