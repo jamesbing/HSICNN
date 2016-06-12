6c7671515bb08bf3093d639162d87fe27f3824c0
jamesbing <lengjiabing@gmail.com>
2016-06-12 16:12:46 +0800

    build model

--- Keras/HSICNN/HSICNN.py
+++ Keras/HSICNN/HSICNN.py
@@ -6,10 +6,26 @@ import numpy
 
 from keras.models import Sequential
 from keras.layers.core import Dense,Dropout,Activation,Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, Convolution1D, MaxPooling1D
 from keras.optimizers import SGD
 
 from keras.utils import np_utils
+
+##################################################
+#This function is used to construct the CNN model#
+##################################################
+def build_CNN_model(layers, loss, optimizer):
+    
+    model = Sequential()
+    for layer in layers:
+        model.add(layer)
+
+    model.compile(loss=loss,optimizer=optimizer)
+    
+    return model
+
+
+
 
 
 
