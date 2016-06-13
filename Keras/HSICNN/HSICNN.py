4f755e6463882c9abb8bfc20f0aa7ef9e6b1ba1c
james leng <lengjiabing@gmail.com>
2016-06-13 10:23:07 +0800

    add load data

--- Keras/HSICNN/HSICNN.py
+++ Keras/HSICNN/HSICNN.py
@@ -33,5 +33,40 @@ def train_model(model, x_train, y _train, x_val, y_val, batch_size,nb_epoch):
              validation_data=(x_val, y_val))
 
 
+################################
+#按照数据预处理的格式，装载数据#
+################################
+def loadData(dataFile, typeId = -1, bShowData = False):
+    data = sio.loadmat(dataFile)
+
+    train_data = data['DataTr']
+    train_label = data['CIdTr'][0,:]
+#    train_label = train_label[0,:]
+#    return train_data,train_label
+    train_set = [train_data, train_label]
+
+    test_data = data['DataTe']
+    test_label = data['CIdTe'][0,:]
+    test_set = [test_data, test_label]
+
+    valid_data = data['DataVa']
+    valid_label = data['CIdVa'][0,:]
+    valid_set = [valid_data, valid_label]
+
+#    def shared_dataset(data_xy, borrow=True):
+#        data_x, data_y = data_xy
+#        shared_x = theano.shared(numpy.asarray(data_x,dtype=theano.config.floatX))
+#        shared_y = theano.shared(numpy.asarray(data_y,dtype='int32'))
+#        return shared_x, shared_y
+
+#    test_set_x, test_set_y = shared_dataset(test_set)
+#    valid_set_x, valid_set_y = shared_dataset(valid_set)
+#    train_set_x, train_set_y = shared_dataset(train_set)
+
+#    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y), (test_set_x, test_set_y)]
+    
+    rval = [(train_data, train_label),(valid_data,valid_label),(test_data,test_label)]
+    return rval
+
 
 
