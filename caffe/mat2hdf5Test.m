50f41afb0bba844e2f4e2cd95ad8a7de194aac63
james <tadakey@qq.com>
2016-10-15 08:58:33 +0800

    Update and rename mat2hdf5Test.mat to mat2hdf5Test.m

--- caffe/mat2hdf5Test.m
+++ caffe/mat2hdf5Test.m
@@ -1,4 +1,4 @@
-%
+%这段matlab脚本的作用是将高光谱图像数据（近邻像素处理后的）训练集组装成caffe认识的hdf5格式的文件
 h5create('test.hd5','/data',[1 1 1584 1050],'Datatype','single');
 h5create('test.hd5','/label',[1 1 1 1050],'Datatype','single');
 test_data = reshape(DataTe, [1 1 1584 1050]);
