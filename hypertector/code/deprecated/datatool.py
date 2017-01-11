2d1a6acef1c56291539eff166351c948556b0d44
jamesbing <tadakey@163.com>
2017-01-11 09:35:50 +0800

    old codes

---
+++ hypertector/code/deprecated/datatool.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+
+# this py file should hold following functions:
+# A grab data from raw dataset files
+# B give infromation to input neighbor strategy, such as 1,4,8 strategy
+# C give information to input ratio of training:testing samples
+# D give information to input what's backend will be used, such as caffe, tehano or tensorflow.
+# according to the A B C and D, construct the proper dataset.
+
+
