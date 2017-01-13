f372f18603045bf0efc921e892a1afcb7cc3688a
jamesbing <tadakey@163.com>
2017-01-13 09:42:18 +0800

    modify hypertector

--- hypertector/code/hypertector.py
+++ hypertector/code/hypertector.py
@@ -10,6 +10,8 @@ from sys import argv
 
 prompt = '>'
 fullLayers = 100
+#mix_model_svm_ratio是为了以后采用组合混合模型的时候，保存一个svm在所有模型中的占比。以后根据需求进行扩充。。。TODO
+mix_model_svm_ratio = 
 file_name, neighbors = data_util.prepare()
 print "now constructing the network..."
 #print "enter the layers each convolutional kernel covers: "
@@ -30,14 +32,36 @@ print "enter the train decay:"
 train_decay = float(raw_input(prompt))
 print "enter the epoches you want the network to be trained:"
 epoches = int(raw_input(prompt))
-print "starting ..."
 
+print "now choose the following strategy after the cnn network been trained:"
+print "#1:train a cnn-svm joint framework;"
+print "#2:train a cnn-rf joint framework;"
+print "#3:train both cnn-svm and cnn-rf joint frameworks;"
+print "#3:TODO: train a mix assemble cnn-classifier model."
+following_strategy = int(raw_input(prompt))
+if following_strategy == 4:
+    print "enter the ratio of svm classifier:"
+    mix_model_svm_ratio = int(row_input(prompt))
+
+
+print "starting ..."
 HSICNN.run_network(file_name, neighbors, maxpoolings, fullLayers,batch_size, learning, train_decay, epoches)
 
-#CNN + SVM
-print "now processing the cnn + svm joint framework..."
-cnnsvm.run(file_name, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
+print "the training of the network have done."
 
-#TODO: CNN + rf
+if following_strategy == 1:
+    #CNN + SVM
+    print "now processing the cnn + svm joint framework..."
+    cnnsvm.run(file_name, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
+elif following_strategy == 2:
+    #CNN + rfind
+    print "now processing the cnn + rf joint framework..."
+    cnnrf.run(file_name, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
+elif following_strategy == 3:
+    #CNN+svm and CNN+RF
+    
+    print "now processing the cnn + svm joint framework..."
+    cnnsvm.run(file_name, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
 
-
+    print "now processing the cnn + rf joint framework..."
+    cnnrf.run(file_name, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
