34d1ff7c7198b2f31c502b3e09d0882b9af688ae
jamesbing <tadakey@163.com>
2017-01-18 23:11:57 +0800

    add functions

--- hypertector/code/hypertector.py
+++ hypertector/code/hypertector.py
@@ -18,7 +18,10 @@ print "now constructing the network..."
 #print "enter the layers each convolutional kernel covers: "
 #neighbors = int(raw_input(prompt))
 neighbors = neighbors + 1
+
 print "the neighbors strategy is: " + str(neighbors)
+print "enter the number of convolutional neurons:"
+neurons = int(raw_input(prompt))
 print "enter the number of neurons after the maxpooling layer:"
 maxpoolings = int(raw_input(prompt))
 print "enter the number of full layers\' neurons, default is 100:"
@@ -38,43 +41,54 @@ print "now choose the following strategy after the cnn network been trained:"
 print "#1:train a cnn-svm joint framework;"
 print "#2:train a cnn-rf joint framework;"
 print "#3:train both cnn-svm and cnn-rf joint frameworks;"
-print "#3:TODO: train a mix assemble cnn-classifier model."
+print "#4:TODO: train a mix assemble cnn-classifier model."
 following_strategy = int(raw_input(prompt))
 if following_strategy == 4:
     print "enter the ratio of svm classifier:"
     mix_model_svm_ratio = int(row_input(prompt))
 
+tress = 0
+if following_strategy == 2 or following_strategy == 3:
+    print "enter the count of trees you want to set in Random Forest:"
+    trees = int(raw_input(prompt))
+
 print "starting ..."
-HSICNN.run_network(file_name, neighbors, maxpoolings, fullLayers,batch_size, learning, train_decay, epoches)
+HSICNN.run_network(file_name, neurons, neighbors, maxpoolings, fullLayers,batch_size, learning, train_decay, epoches)
 
 print "the training of the network have done."
 
 if following_strategy == 1:
     #CNN + SVM
     print "now processing the cnn + svm joint framework..."
-    cnnsvm.run(file_name, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
+    cnnsvm.run(file_name, neurons, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
 elif following_strategy == 2:
     #CNN + rfind
     print "now processing the cnn + rf joint framework..."
-    cnnrf.run(file_name, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
+#    print "enter the count of trees you want to set in Random Forest:"
+#    trees = int(raw_input(prompt))
+    cnnrf.run(file_name,trees, neurons, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
 elif following_strategy == 3:
     #CNN+svm and CNN+RF
     
     print "now processing the cnn + svm joint framework..."
-    cnnsvm.run(file_name, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
+    cnnsvm.run(file_name, neurons, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
 
     print "now processing the cnn + rf joint framework..."
-    cnnrf.run(file_name, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
+    cnnrf.run(file_name, trees, neurons, neighbors, maxpoolings, fullLayers, batch_size, learning, train_decay)
 
 file = open(file_name + "_experiment_description.txt", 'w')
-file.write("-------------Experiment Description-------------")
+file.write("-------------Experiment Description-------------\n")
 file.write("Data set:" + file_name + "#\n")
 file.write("neighbor strategy:" + str(neighbors) + "#\n")
+file.write("Convolutional Neurons:" + str(neurons) + "#\n")
 file.write("Max Polling Layer Neuron number:" + str(maxpoolings) + "#\n")
-file.write("Full Layer Neuron number:" + str(tempfullLayers) + "#\n")
+file.write("Full Layer Neuron number:" + str(fullLayers) + "#\n")
 file.write("Batch size of SGD training:" + str(batch_size) + "#\n")
 file.write("Training epoches of deep CNN:" + str(epoches) + "#\n")
 file.write("Learning ratio:" + str(learning) + "#\n")
 file.write("Train decay:" + str(train_decay) +"#\n")
+if following_strategy == 2 or following_strategy == 3:
+    file.write("Number of trees in random forest: " + str(trees) + "#\n")
+file.write("===============================================")
 
 
