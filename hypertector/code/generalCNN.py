b57436f0b85104c8b89146175c27ada007a3cf97
jamesbing <tadakey@163.com>
2017-02-10 13:43:39 +0800

    update instructions of the purpose of the general purpose CNN network

--- hypertector/code/generalCNN.py
+++ hypertector/code/generalCNN.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
-
+# this piece of code is used of to construt a general purpose cnn network.
+# the main idea of the general pupose CNN is that :
+# the steps of the general purpose CNN are as following:
+# A: construct a CNN acoording to the 
