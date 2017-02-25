682761458c029cd6cd5ad1d1a443502099ca133a
jamesbing <tadakey@163.com>
2017-02-25 17:29:50 +0800

    update

--- hypertector/code/generalCNN.py
+++ hypertector/code/generalCNN.py
@@ -3,4 +3,4 @@
 # this piece of code is used of to construt a general purpose cnn network.
 # the main idea of the general pupose CNN is that :
 # the steps of the general purpose CNN are as following:
-# A: construct a CNN acoording to the  
+# A: construct a CNN acoording to the given parameter... 
