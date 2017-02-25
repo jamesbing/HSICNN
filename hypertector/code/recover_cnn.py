db868b8a00ae48d5e54283f34a15e52ca1230259
jamesbing <tadakey@163.com>
2017-02-25 17:28:22 +0800

    to recover the network

--- hypertector/code/recover_cnn.py
+++ hypertector/code/recover_cnn.py
@@ -2,3 +2,5 @@
 # coding=utf-8
 #this file is used to recover the network from the already exsist cnn model file.
 
+def recover():
+    print "recovering the networking with the trained parameters..."
