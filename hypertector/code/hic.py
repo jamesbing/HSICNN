3aac83eac57eb660f75168c998bdbec3dac8225e
jamesbing <tadakey@163.com>
2017-03-01 10:01:15 +0800

    add the hic file

---
+++ hypertector/code/hic.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# coding=utf-8
+# this network is based on the network proposed by Prof. Baigang on 2017, 3, 1 morning.
+# this network should also be the prototype of other non-image based data classification tasks.
