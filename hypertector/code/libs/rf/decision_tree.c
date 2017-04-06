b18502c5e3d4d555d6bd43c5264fdea370a51499
jamesbing <lengjiabing@gmail.com>
2017-04-06 15:41:53 +0800

    modify

--- hypertector/code/libs/rf/decision_tree.c
+++ hypertector/code/libs/rf/decision_tree.c
@@ -14,8 +14,8 @@
 typedef enum {false = 0, true = 1} bool;
 
 //some constant variables to construct a decision tree
-#define Max_Samples 100000  //the max size of sampls
-#define Max_Attributes 300  //the max number of a certain attribute
+#define Max_Samples 100000  //the max size of sampls  the M
+#define Max_Attributes 300  //the max number of a certain attribute  the N
 #define Len 20
 
 //define the structure of a node in decision tree
@@ -27,11 +27,41 @@ struct TreeNode{
 };
 
 /*
- *构造决策树
+ *判断是否所有样本都属于同一类
  *
  */
-TreeNode* ConstructTree()
+ bool same_class()
+ {
 
-int main(){
+ }
+
+
+
+/*
+ *构造决策树
+ ==================
+ Parameters:
+ D:训练数据集
+ A:特征集
+ threashold:阈值
+ ==================
+ Return:
+ TreeNode T:学习到的决策树
+ *
+ */
+ struct TreeNode* BuildTree(int D, int A[], int threshold)
+ {
+     struct TreeNode *T;
+    
+     //判断D中的所有实例是否属于同一类C，若是，则置T为但节点数，并将C作为该节点的类，返回T
+    if (same_class())
+     //
+
+
+     return T;
+ }
+
+int main()
+{
     printf("Construct decision tree...\n");
 }
