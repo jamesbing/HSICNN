2961faa0978fd51957f264e4544243e1b521aa6d
jamesbing <lengjiabing@gmail.com>
2017-04-06 18:51:28 +0800

    modify

--- hypertector/code/libs/rf/decision_tree.c
+++ hypertector/code/libs/rf/decision_tree.c
@@ -17,6 +17,7 @@ typedef enum {false = 0, true = 1} bool;
 #define Max_Samples 100000  //the max size of sampls  the M
 #define Max_Attributes 300  //the max number of a certain attribute  the N
 #define Len 20
+#define dimension 10
 
 //define the structure of a node in decision tree
 //split_attribute:表示该节点用于划分数据的特征
@@ -27,6 +28,12 @@ struct TreeNode{
     bool is_leaf_node;
     int class_id;
     struct TreeNode* child[Max_Attributes];
+};
+
+//define the structure of a sample
+struct sample{
+    int class;
+    float* data[dimension];
 };
 
 /*
@@ -47,6 +54,24 @@ struct TreeNode{
 
 
      return class;
+ }
+
+ /**
+  *用于取得划分样本的最佳特征，采用C4.5算法
+  */
+ int get_split_attribute(D, A)
+ {
+     int class_number = 2048;
+
+     return class_number;
+ }
+
+ /**
+  *计算信息增益比
+  */
+ float entropy_ratio()
+ {
+     return 1;
  }
 
 /*
@@ -71,7 +96,8 @@ struct TreeNode{
      {
          T->is_leaf_node = true;
          //TODO：这儿应该是将类别取出赋给class_id，但是由于数据格式不确定，暂时先空着
-         T->class_id = D[0]; 
+         int temp_class = 10;
+         T->class_id = temp_class; 
          return T;
          
      }
@@ -82,10 +108,19 @@ struct TreeNode{
          T->class_id = get_most_class(D);
          return T;
      }
-
     //上述两条判断都不成立，则进行数据集的分裂
     //挑选数据集中用于划分节点的属性
-    T->
+    int chosen_attribute = get_split_attribute(D, A);
+    float information_gain_ratio = entropy_ratio(chosen_attribute);
+    if (information_gain_ratio < threshold)
+     {
+        T->is_leaf_node = true;
+        T->class_id = get_most_class(D);
+        return T;
+     }else{
+         printf("重新划分数据....\n");
+        
+     }
      
      return T;
  }
@@ -93,6 +128,8 @@ struct TreeNode{
 int main()
 {
     //读取输入训练数据集
+//    float data1[2][1] = [[2.0],[0.21,0.33,0.87,0.77,0.66]];
+    
     
     printf("Construct decision tree...\n");
 }
