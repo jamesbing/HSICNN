0da81d8e5fa58a12474db6fb528b7cc96785f23d
jamesbing <tadakey@163.com>
2017-05-03 09:51:55 +0800

    new file to support the check caffe solver model

---
+++ vgg/tools/check_caffe_solver_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+
+context = '/home/para/caffe/'
+sys.path.insert(0,context + '/python')
+import caffe
+
+import numpy as np
+root = '/home/para/gitstore/HSICNN/vgg/'
+deploy = root + 'train_test.prototxt'
+caffe_model_root = root + 'experiment_results/'
+
+def Test(model_sequence_number):
+    caffe_model = caffe_model_root + model_sequence_number + '/_iter_10000.caffemodel'
+    net = caffe.Net(deploy,caffe_model,caffe.TEST)
