c6b95a1f5889292d3269ded4797d7c33b8ec5b4b
jamesbing <tadakey@163.com>
2017-05-03 09:59:21 +0800

    modify

--- vgg/tools/check_caffe_solver_state.py
+++ vgg/tools/check_caffe_solver_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 import os
-
+import sys
 context = '/home/para/caffe/'
 sys.path.insert(0,context + '/python')
 import caffe
@@ -14,3 +14,9 @@ caffe_model_root = root + 'experiment_results/'
 def Test(model_sequence_number):
     caffe_model = caffe_model_root + model_sequence_number + '/_iter_10000.caffemodel'
     net = caffe.Net(deploy,caffe_model,caffe.TEST)
+
+    out = net.forward()
+    print out
+
+if '__name__' == '__main__':
+    Test(1)
